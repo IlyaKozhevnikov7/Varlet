e34417aef3bb15295b63cbb3006f26f4ac85bad5
IlyaKozhevnikov7 <skeletor1215@gmail.com>
2022-06-28 23:49:51 +0300

    Added framework header

---
+++ Varlet/Source/Include/VarletFramework.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// math
+#include "../../Dependencies/GLM/include/glm/glm.hpp"
+
+// utils
+#include "Core/Delegate.h"
+#include "Core/Logger.h"
+
+// components
+#include "Scene/Entity.h"
+#include "Scene/Scene.h"
+#include "Scene/Component.h"
+#include "Scene/Camera.h"
+#include "Scene/MeshRenderer.h"
+#include "Scene/Transform.h"
+
+// input
+#include "Input/Input.h"
+#include "Platform/Windows/GLFWInput.h" // for key enums
